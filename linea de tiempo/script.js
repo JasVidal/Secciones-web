17cf9d316422ac2fd131c5419567e5ad2c99b97f
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-04-13 23:25:48 -0500

    nueva actualización

--- linea de tiempo/script.js
+++ linea de tiempo/script.js
@@ -1,34 +1,34 @@
-const progress = document.getElementById('progreso');
-const ant = document.getElementById('ant');
-const sig = document.getElementById('sig');
-const circulos = document.querySelectorAll('.circulo');
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
 
 let currentActive = 1;
 
-sig.addEventListener('click', () => {
-    currentActive++;
-    if (currentActive > circulos.length) currentActive = circulos.length;
-    update();
+next.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circles.length) currentActive = circles.length;
+  update();
 });
 
-ant.addEventListener('click', () => {
-    currentActive--;
-    if (currentActive < 1) currentActive = 1;
-    update();
+prev.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) currentActive = 1;
+  update();
 });
 
 const update = () => {
-    circulos.forEach((circulo, index) => {
-        if (index < currentActive) circulo.classList.add('activo');
-        else circulo.classList.remove('activo');
-    });
-    const activos = document.querySelectorAll('.activo');
-    progress.style.width =
-        ((activos.length - 1) / (circulos.length - 1)) * 100 + '%';
-    if (currentActive === 1) prev.disabled = true;
-    else if (currentActive === circulos.length) next.disabled = true;
-    else {
-        ant.disabled = false;
-        sig.disabled = false;
-    }
+  circles.forEach((circle, index) => {
+    if (index < currentActive) circle.classList.add("active");
+    else circle.classList.remove("active");
+  });
+  const actives = document.querySelectorAll(".active");
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+  if (currentActive === 1) prev.disabled = true;
+  else if (currentActive === circles.length) next.disabled = true;
+  else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
 };
