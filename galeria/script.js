4a9cd59a26a149014905e707556d43bd8a9e7c2b
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-04-13 19:43:58 -0500

    agregando actualizaciones

--- galeria/script.js
+++ galeria/script.js
@@ -2,12 +2,13 @@ const panels = document.querySelectorAll(".panel");
 
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
+    removeActiveClasses();
     panel.classList.add("active");
   });
 });
 
 const removeActiveClasses = () => {
-    panels.forEach((panel) => {
-      panel.classList.remove("active");
-    });
-  };
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
